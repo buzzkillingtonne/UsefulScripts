681c4836452925fea9010c65bfc7edc316965250
buzzkillingtonne <52645729+buzzkillingtonne@users.noreply.github.com>
2021-11-17 19:58:51 -0700

    Update ckb-next-daemon-fix-logon-script.sh

--- ckb-next-daemon-fix-logon-script.sh
+++ ckb-next-daemon-fix-logon-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#Place this folder in home directory and set as a logon script (such as in KDE's setting application) to prevent keyboard from not working at logon
+# Place this folder in home directory and set as a logon script (such as in KDE's setting application) to prevent keyboard from not working at logon
+# Disable ckb-next daemon with systemctl disable ckb-next-daemon.service (this script starts it at logon)
 sudo systemctl start ckb-next-daemon.service 
 ckb-next --background
