80f3db24d808f32ec415c9e8f20d75a4e79eef15
buzzkillingtonne <52645729+buzzkillingtonne@users.noreply.github.com>
2021-12-18 00:37:52 -0700

    Update proxmox_backup_client.sh

--- proxmox_backup_client.sh
+++ proxmox_backup_client.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## I would like to thenk Turnicus on the proxmox forums for creating the basis of this script:
+## https://forum.proxmox.com/threads/proxmox-backup-client-bash-script-for-automated-backups-of-laptop.78358/
+
 ### Depedencies #########################################
 # proxmox-backup-client					#
 # postfix (for email alerts)				#
