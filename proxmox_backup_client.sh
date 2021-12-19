f80818c965c2fc01aad06631822b4d7a21f16200
buzzkillingtonne <52645729+buzzkillingtonne@users.noreply.github.com>
2021-12-18 19:36:45 -0700

    Update proxmox_backup_client.sh

--- proxmox_backup_client.sh
+++ proxmox_backup_client.sh
@@ -29,8 +29,9 @@ _pbs_ip_address='X.X.X.X'
 _pbs_user='username@pbs!username-api'
 _pbs_datastore=''
 _pbs_client=$(hostname)
-# _pbs_backup_dir backs up root by default, change to the directory desired eg. /home
-_pbs_backup_dir='/'
+# _pbs_backup_dir backs up root by default, change to the directory desired eg. home
+# (!IMPORTANT! you cannot put the preceeding / in the variable, it will fail.)
+_pbs_backup_dir=''
 export PBS_PASSWORD=<GET THIS FROM YOUR PBS SERVER>
 export PBS_FINGERPRINT=<GET THIS FROM YOUR PBS SERVER>
 ###################################
@@ -45,24 +46,28 @@ function run_backup {
 if [[ $_seconds_elapsed_since_last_backup -ge 86400 ]]; then
 
 	printf -- "- Starting the PBS backup" | systemd-cat
-	proxmox-backup-client backup $_pbs_backup_dir.pxar:$_pbs_backup_dir --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore
-	printf -- "- PBS backup completed" | systemd-cat
-	printf -- "- $0 completed at `date`" | systemd-cat
-	
-	printf -- "- Gathering information for email" | systemd-cat
+	proxmox-backup-client backup $_pbs_backup_dir.pxar:/$_pbs_backup_dir --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore
+	if [[ $? == 0  ]]; then
+		printf -- "- PBS backup completed" | systemd-cat
+		printf -- "- $0 completed at `date`" | systemd-cat
+		
+		printf -- "- Gathering information for email" | systemd-cat
 # This must be the the for of 'UPID:yourpbdserverhostname:0002A09D:0AEE0BA0:0000049B:61BD33FA:backup:yourbackuprepositoryname\x3ahost-yourpbsusername\x2ddesktop:' without quotes
 # It is important that backup jobs for different computers use different pbs user names and api's, this script finds the backup to report on based on the last backup taken with that username.
-	_backup_upid=$(proxmox-backup-client task list --all --limit 2 --output-format json --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore | jq -r '[ .[]."upid" ]' | grep backup | tr -d '"''  ' | tr -s '\\' '\\'  | awk -F 'x2ddesktop:' '{print $1"x2ddesktop:"}')
-	_last_backup_job_status=$(proxmox-backup-client task log $_backup_upid$_pbs_user: --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore)
-	_job_status=$(proxmox-backup-client task log $_backup_upid$_pbs_user: --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore | tail -1)
-	
-	printf -- "- Clearing enviroment variables" | systemd-cat
-	export PBS_PASSWORD=
-	export PBS_FINGERPRINT=
-	
-	printf -- "- Sending email" | systemd-cat
-	mail -s "Proxmox Backup Client $_pbs_client status $_job_status"  $EMAIL <<< "$_last_backup_job_status"
-	printf -- "- $0 completed at `date`" | systemd-cat
+		_backup_upid=$(proxmox-backup-client task list --all --limit 2 --output-format json --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore | jq -r '[ .[]."upid" ]' | grep backup | tr -d '"''  ' | tr -s '\\' '\\'  | awk -F 'x2ddesktop:' '{print $1"x2ddesktop:"}')
+		_last_backup_job_status=$(proxmox-backup-client task log $_backup_upid$_pbs_user: --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore)
+		_job_status=$(proxmox-backup-client task log $_backup_upid$_pbs_user: --repository $_pbs_user@$_pbs_ip_address:$_pbs_datastore | tail -1)
+		
+		printf -- "- Clearing enviroment variables" | systemd-cat
+		export PBS_PASSWORD=
+		export PBS_FINGERPRINT=
+		printf -- "- Sending email" | systemd-cat
+		mail -s "Proxmox Backup Client $_pbs_client status $_job_status"  $EMAIL <<< "$_last_backup_job_status"
+		printf -- "- $0 completed at `date`" | systemd-cat
+	else
+		printf -- "- proxmox-backup-client failed with exit status $?"
+	fi
+		
 else
 	printf -- "- $_seconds_elapsed_since_last_backup seconds elapsed since last backup, must be greater than 86400, exiting" | systemd-cat
 fi
